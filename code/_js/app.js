6e18ef1ad62f49ab3ee68b9a7e66a5d69a1c8ba3
Victor Martins <martinsvictor6781@gmail.com>
2022-02-21 16:51:35 -0300

    Adição da criação de uma despesa como objeto

--- code/_js/app.js
+++ code/_js/app.js
@@ -1,10 +1,24 @@
 const enumTipo = {"alimentação": 0, "lazer": 1, "educação": 2, "saúde": 3, "trasporte": 4  }
 
-class despesa{
+class Despesa{
     constructor(date, tipo, valor, descricao){
         this.date = date
         this.tipo = tipo
         this.valor = valor
         this.descricao = descricao
     }
+}
+let despesas = []
+
+
+let btnCadastrar = document.getElementById("btn-cadastrar")
+btnCadastrar.addEventListener("click", cadastrar)
+
+function cadastrar(){
+    var date = document.getElementById("input-date").value
+    var tipo = document.getElementById("input-select").value
+    var valor = document.getElementById("input-value").value
+    var descricao = document.getElementById("input-descricao").value
+
+    let despesa = new Despesa(date, tipo, valor, descricao)
 }
