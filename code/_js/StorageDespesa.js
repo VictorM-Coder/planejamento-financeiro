4474cb75f41c8f1ba7541f2d5979a09840e4e9c3
Victor Martins <martinsvictor6781@gmail.com>
2022-02-25 20:06:25 -0300

    Update StorageDespesa.js

--- code/_js/StorageDespesa.js
+++ code/_js/StorageDespesa.js
@@ -11,4 +11,13 @@ class StorageDespesa{
         localStorage.setItem(++id, JSON.stringify(despesa))
         localStorage.setItem("id", id)
     }
+
+    getDespesas(){
+        let despesas = []
+        for(let cont = 1; cont < localStorage.length; cont++){
+            despesas.push(localStorage.getItem(cont))
+        }
+
+        return despesas
+    }
 }
