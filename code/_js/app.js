ca34b3216b502ab9f78c78140569d45d87655d21
Victor Martins <martinsvictor6781@gmail.com>
2022-03-25 15:17:08 -0300

    refactor modais
    validação do tipo de modal via código

--- code/_js/app.js
+++ code/_js/app.js
@@ -1,14 +1,21 @@
 let storage = new StorageDespesa()
 let btnCadastrar = document.getElementById("btn-cadastrar")
+let modalTitle = document.getElementById("modal-title")
+let modalMessage = document.getElementById("modal-message")
+let modalButton = document.getElementById("modal-button")
+
 btnCadastrar.addEventListener("click", cadastrar)
 
 function cadastrar(){
+
     if(getDespesa().valuesIsNotEmpty()){
         storage.salvarDespesa(getDespesa())
-        $('#success-modal').modal('show')
+        setModalSuccess()
     }else{
-        $('#fail-modal').modal('show')
+        setModalFail()
     }
+
+    $('#modal-alert').modal('show')
 }
 
 function getDespesa(){
@@ -18,4 +25,18 @@ function getDespesa(){
     var descricao = document.getElementById("input-descricao").value
 
     return new Despesa(date, tipo, valor, descricao) 
+}
+
+function setModalSuccess(){
+    modalTitle.innerText = "Cadastro realizado com sucesso"
+    modalTitle.className += " text-success"
+    modalMessage.innerText = "Sua despesa foi cadastrada com exito"
+    modalButton.className += " btn-success"
+}
+
+function setModalFail(){
+    modalTitle.innerText = "Falha no cadastro"
+    modalTitle.className += " text-danger"
+    modalMessage.innerText = "Houve um erro ao tentarmos cadastrar sua despesa, tente novamente"
+    modalButton.className += " btn-danger"
 }
